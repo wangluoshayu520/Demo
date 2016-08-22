dba8a56c6a4c9c3bb9116931a3aafd4d3562d753
wangluoshayu520 <1366881012@qq.com>
2016-08-22 09:12:42 +0800

    Create Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,12 @@
+class Demo｛
+  public static void main(String[] args){
+    int[] arr=new int[2];
+    int[0]=1;
+    int[1]=2;
+    for(int i=0;i<arr.length;i++){
+    System.out.println(arr[i]);
+    }
+    
+  }
+  
+｝
